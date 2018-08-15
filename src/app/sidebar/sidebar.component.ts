31b85fcbb8bfdd657dc69b62d17dce85e66881eb
Jie Li <jl@ocg.de>
2018-08-15 15:52:03 +0200

    Added bg color for focused sidebar item

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { ConfigService } from '../core/services/config.service';
 
@@ -10,9 +11,14 @@ import { ConfigService } from '../core/services/config.service';
 export class SidebarComponent implements OnInit {
   sidebarItems: Array<any> = [];
 
-  constructor(private config: ConfigService) {}
+  constructor(private config: ConfigService, private router: Router) {}
 
   ngOnInit() {
     this.sidebarItems = this.config.getConfig('sidebarItems', []);
+  }
+
+  isFocusedItem(item: any) {
+    const retVal = this.router.url === '/app/' + item.path;
+    return retVal;
   }
 }
